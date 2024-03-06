8553d971dea912be061ed56e9ed2d5deb6d8fc41
Willie Yeh <willieyeh65@gmail.com>
2024-03-05 16:32:07 -0800

    Got navbar working with search

---
+++ assets/css/script.js
@@ -0,0 +1,10 @@
+let fiveData;
+let todayData;
+
+async function getApi(event){
+    event.preventDefault()
+    let cityName = document.querySelector('cityName').value
+
+}
+// `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a4d20a728b63e7469a525e2ba3e247e1`
+
