16840a4869576c631067291719b6d280377705c6
Willie Yeh <willieyeh65@gmail.com>
2024-03-05 22:15:53 -0800

    changed minor api coding

--- assets/css/script.js
+++ assets/css/script.js
@@ -4,7 +4,7 @@ let todayData;
 async function getApi(event){
     event.preventDefault()
     const cityName = document.querySelector('#cityName').value
-    const weather = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a4d20a728b63e7469a525e2ba3e247e1`
+    const weather = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=a4d20a728b63e7469a525e2ba3e247e1`
 
 
 
